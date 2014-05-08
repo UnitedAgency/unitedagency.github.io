71dc5b48e27d7d5ebd9e8ad956beb663bb7863e4
Jabran Rafique <hello@jabran.me>
2014-05-08 18:11:33 +0100

    update responsive behaviour for images

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,5 +50,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+	grunt.registerTask('default', ['sass', 'watch']);
 }; 
